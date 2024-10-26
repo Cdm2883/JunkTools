2dc59335d4f319f77544765eda13afeb2952095e
Cdm2883 <Cdm2883@hotmail.com>
2024-10-26 19:24:18 +0800

    build: setup kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
-//    alias(libs.plugins.) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
 }
